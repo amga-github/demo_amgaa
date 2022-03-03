0a95c1ee7bac06655f8ae5bad83f395768651ee8
Amgalanbayar Jargalsakhan <j.amgaa0206.mn@gmail.com>
2022-03-03 19:00:34 +0800

    f

---
+++ src/main/java/mn/amg/f/cs314_b170910805_lab1/Application.java
@@ -0,0 +1,13 @@
+package mn.amg.f.cs314_b170910805_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
